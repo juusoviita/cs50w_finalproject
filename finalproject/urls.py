bd8d812f5f9c60f4a1fd065949810929130b0858
juusoviita <juusoviita@gmail.com>
2020-11-24 14:03:06 +0200

    started work on structure

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('workably/', include('workably.urls'))
+    path('', include('workably.urls'))
 ]
--- workably/views.py
+++ workably/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.urls import reverse
+
+import json
+
+# from .models import which ever models have been created
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "workably/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "workably/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "workably/login.html")
+
+
+def logout_view(request):
+    # insert a change here
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
